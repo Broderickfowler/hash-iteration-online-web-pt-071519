de7268f4004be4bd1280bb66b0cc7b92f8066918
Broderick  Fowler Jr <broderickfowler@outlook.com>
2019-08-03 16:06:45 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,8 +6,9 @@
 
 def happy_birthday(birthday_kids)
   # add your code snippet here!
-  #we need to puts method that says Happy Birthday #{kids_name}! You are now #{age} yo!
+  #
   birthday_kids.each do |kids_name, age|
+    #we need to puts method that says Happy Birthday #{kids_name}! You are now #{age} yo!
     puts "Happy Birthday #{kids_name}! You are now #{age} year old!"
   end
 end
