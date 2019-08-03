26182acfc329fc7e447e6f8b908497d295458ce8
Broderick  Fowler Jr <broderickfowler@outlook.com>
2019-08-03 15:54:54 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -2,7 +2,7 @@ def select_winner(passengers)
   passengers.each do |suite, name|
     puts "#{suite}: #{name}"
     if suite == :suite_a && name.start_with?("A")
-       puts "winner winner = name"
+       puts 
     end
   end
 end
