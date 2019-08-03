c457007793e772bbc56fd47da83f28775a083d77
Broderick  Fowler Jr <broderickfowler@outlook.com>
2019-08-03 15:39:56 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,8 +1,8 @@
 def select_winner(passengers)
-winner = ""
+passengers = ""
 passengers.each do |suite, name|
   if suite == :suite_a && name.start_with?("A")
-    winner = name
+    passengers = name
 
 end
  
